6ba9f93b7a7641575f793a00f157dc1e23380b2e
Adrian Paras <aparas@terpmail.umd.edu>
2024-02-28 11:37:01 -0500

    Add backend directory

---
+++ projectmanager-backend/src/test/java/com/example/projectmanagerbackend/ProjectmanagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmanagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
