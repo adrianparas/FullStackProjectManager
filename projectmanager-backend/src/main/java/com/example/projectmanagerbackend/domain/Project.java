3b9033e54de5ac25ad24b0e3e0e70c55be6e407b
Adrian Paras <aparas@terpmail.umd.edu>
2024-02-28 12:01:47 -0500

    updating project class and adding repository

--- projectmanager-backend/src/main/java/com/example/projectmanagerbackend/domain/Project.java
+++ projectmanager-backend/src/main/java/com/example/projectmanagerbackend/domain/Project.java
@@ -1,5 +1,42 @@
 package com.example.projectmanagerbackend.domain;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Project {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date start_date;
+    private Date end_date;
+
+    private Date created_At;
+    private Date updated_At;
+
+    @PrePersist
+    protected void onCreate() {
+        this.created_At = new Date();
+    }
+
+    @PrePersist
+    protected void onUpdate() {
+        this.updated_At = new Date();
+    }
+
+
     
 }
---
+++ projectmanager-backend/src/main/java/com/example/projectmanagerbackend/repository/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.example.projectmanagerbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.projectmanagerbackend.domain.Project;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    
+} 
